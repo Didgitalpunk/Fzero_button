ea466247de52c6a3c99077412aa54e6c763f1d34
Didgitalpunk <didgitalpunk@gmail.com>
2023-01-28 18:37:03 +0100

    fixed button detect maybe?

--- button.c
+++ button.c
@@ -17,12 +17,11 @@ int button_app() {
     furi_hal_light_set(LightBlue, 0);
 
     while(furi_hal_power_is_charging()) {
+        furi_check(furi_message_queue_get(event_queue, &event, FuriWaitForever) == FuriStatusOk);
         if(event.key == InputKeyOk) // red light
         {
             furi_hal_light_set(LightRed, 255);
-
         }
-        
     }
     furi_hal_light_set(LightRed, 0);
     return 0;
