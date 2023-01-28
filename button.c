34d1841c1f678ddf2b8fbac5f4038d78e81467a2
Didgitalpunk <didgitalpunk@gmail.com>
2023-01-28 18:19:27 +0100

    more testing

--- button.c
+++ button.c
@@ -8,19 +8,22 @@ Simple App, an example program for the Flipper Zero
 #include <furi_hal_power.h> // lib for power detection
 #include <gui/gui.h> // unused for now
 #include <input/input.h> // lib for keypress detection
+#include <locale/locale.h>
 
 InputEvent event;
 
 int button_app() {
+    furi_hal_light_set(LightGreen, 0);
+    furi_hal_light_set(LightBlue, 0);
+
     while(furi_hal_power_is_charging()) {
         if(event.key == InputKeyOk) // red light
         {
             furi_hal_light_set(LightRed, 255);
-        } else {
-            furi_hal_light_set(LightBlue, 0);
-        }
 
-        furi_hal_light_set(LightRed, 0);
+        }
+        
     }
+    furi_hal_light_set(LightRed, 0);
     return 0;
 }
