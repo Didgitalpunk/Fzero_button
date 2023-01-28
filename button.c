0c26ef06e05af5919f4c795b3ef509927897a9aa
Didgitalpunk <didgitalpunk@gmail.com>
2023-01-28 18:01:40 +0100

    fixed simple_app to button_app in button.c

--- button.c
+++ button.c
@@ -8,11 +8,10 @@ Simple App, an example program for the Flipper Zero
 #include <furi_hal_power.h> // lib for power detection
 #include <gui/gui.h> // unused for now
 #include <input/input.h> // lib for keypress detection
-#include <locale/locale.h>
 
 InputEvent event;
 
-int simple_app() {
+int button_app() {
     while(furi_hal_power_is_charging()) {
         if(event.key == InputKeyOk) // red light
         {
