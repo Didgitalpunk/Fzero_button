a8bfc4abaddea544255ca026be3d9aa0b9a9d703
Didgitalpunk <71613063+Didgitalpunk@users.noreply.github.com>
2023-02-17 16:23:24 +0100

    tryed to make it work using tutorial code

--- button_test.c
+++ button_test.c
@@ -1,74 +1,8 @@
-#include <furi.h>
-#include <furi_hal.h>
-
-#include <gui/gui.h>
-#include <input/input.h>
-#include <notification/notification_messages.h>
-
-void vibro_test_draw_callback(Canvas* canvas, void* ctx) {
-    UNUSED(ctx);
-    canvas_clear(canvas);
-    canvas_set_font(canvas, FontPrimary);
-    canvas_draw_str(canvas, 2, 10, "Vibro application");
-    canvas_set_font(canvas, FontSecondary);
-    canvas_draw_str(canvas, 2, 22, "Press OK turns on vibro");
-    canvas_set_font(canvas, FontSecondary);
-    canvas_draw_str(canvas, 2, 34, "Release OK turns off vibro");
-}
-
-void vibro_test_input_callback(InputEvent* input_event, void* ctx) {
-    furi_assert(ctx);
-    FuriMessageQueue* event_queue = ctx;
-    furi_message_queue_put(event_queue, input_event, FuriWaitForever);
-}
-
-int32_t vibro_test_app(void* p) {
-    UNUSED(p);
-    FuriMessageQueue* event_queue = furi_message_queue_alloc(8, sizeof(InputEvent));
-
-    // Configure view port
-    ViewPort* view_port = view_port_alloc();
-    view_port_draw_callback_set(view_port, vibro_test_draw_callback, NULL);
-    view_port_input_callback_set(view_port, vibro_test_input_callback, event_queue);
-
-    // Register view port in GUI
-    Gui* gui = furi_record_open(RECORD_GUI);
-    gui_add_view_port(gui, view_port, GuiLayerFullscreen);
-
-    NotificationApp* notification = furi_record_open(RECORD_NOTIFICATION);
-
-    InputEvent event;
-
-    while(furi_message_queue_get(event_queue, &event, FuriWaitForever) == FuriStatusOk) {
-        if(event.type == InputTypeShort && event.key == InputKeyBack) {
-            notification_message(notification, &sequence_reset_vibro);
-            notification_message(notification, &sequence_reset_green);
-            break;
-        }
-        if(event.key == InputKeyOk) {
-            if(event.type == InputTypePress) {
-                notification_message(notification, &sequence_set_vibro_on);
-                notification_message(notification, &sequence_set_green_255);
-            } else if(event.type == InputTypeRelease) {
-                notification_message(notification, &sequence_reset_vibro);
-                notification_message(notification, &sequence_reset_green);
-            }
-        }
-    }
-
-    gui_remove_view_port(gui, view_port);
-    view_port_free(view_port);
-    furi_message_queue_free(event_queue);
-
-    furi_record_close(RECORD_NOTIFICATION);
-    furi_record_close(RECORD_GUI);
-
-    return 0;
-}
 /*
 This program is free software, but comes with absolutely NO WARRANTY
 Simple App, an example program for the Flipper Zero
 */
+#include <stdio.h>
 #include <furi.h>
 #include <furi_hal.h>
 #include <furi_hal_light.h> // lib for the front LED
@@ -77,26 +11,49 @@ Simple App, an example program for the Flipper Zero
 #include <input/input.h>    // lib for keypress detection
 #include <locale/locale.h>
 
-InputEvent event;
-FuriMessageQueue* event_queue = furi_message_queue_alloc(8, sizeof(InputEvent));
+// (*1) This will be the label that tells you which keypad-button is pressed.
+char* currentKeyPressed;
+int BUFFER = 10;
 
-static void input_callback(InputEvent* input_event, void* ctx) {
-    furi_assert(ctx);
-    FuriMessageQueue* event_queue = ctx;
-    furi_message_queue_put(event_queue, input_event, FuriWaitForever);
-}
 
-int button_app() {
+int button_app(void* p) {
     furi_hal_light_set(LightGreen, 0);
     furi_hal_light_set(LightBlue, 0);
+    furi_hal_light_set(LightRed, 0);
 
-    while(furi_hal_power_is_charging()) {
+
+    UNUSED(p);
+    // Initialization of (*1)
+    // corrected by an meitwouldseem, thank you man !
+    currentKeyPressed = "NONE";
+
+    InputEvent event;
+    FuriMessageQueue* event_queue = furi_message_queue_alloc(8, sizeof(InputEvent));
+
+
+    while(1){
         furi_check(furi_message_queue_get(event_queue, &event, FuriWaitForever) == FuriStatusOk);
-        if(event.key == InputKeyOk) // red light
-        {
-            furi_hal_light_set(LightRed, 255);
+        if(event.type == InputTypeShort && event.key == InputKeyBack) {
+            furi_hal_light_set(LightRed, 0);
+            furi_hal_light_set(LightGreen, 0);
+            break;
+        }
+        if(event.key == InputKeyOk) {
+            
+                furi_hal_light_set(LightGreen,255);
+                furi_hal_light_set(LightRed, 0);
+            } else{
+                furi_hal_light_set(LightRed, 255);
+                furi_hal_light_set(LightGreen, 0);
+            }
+            
+            if(!furi_hal_power_is_charging()) {     // exit upon disconnecting from USB power
+            furi_hal_light_set(LightBacklight, 255);
+            break;
         }
     }
-    furi_hal_light_set(LightRed, 0);
+
+    // Freeing up the rest of memory from all the unused resources.
+    furi_message_queue_free(event_queue);
     return 0;
 }
